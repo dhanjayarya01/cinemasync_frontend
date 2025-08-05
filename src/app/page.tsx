596fd55eeefb01843f04cb2b6e4136465d7840aa
dhanjayarya01 <dhanjayarya20@gmail.com>
2025-08-05 21:50:47 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+export default function page() {
+  return (
+    <div>page
+    <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Click Me
+
+    </Button>
+    </div>
+  )
+}
