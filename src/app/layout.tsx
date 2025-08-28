574ea3b7b2452f548bc6cc76524951070cc25f3e
dhanjayarya <dhanjayarya20@gmail.com>
2025-08-28 13:53:04 +0530

    testing chat ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'CinemaSync',
+  description: 'Watch movies together in perfect sync with friends',
+  generator: 'CinemaSync',
 }
 
 export default function RootLayout({
